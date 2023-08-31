4d129f689eacd62fab37766a1310f5fcbedf1db6
Mahmoud <re00zq@gmail.com>
2023-08-31 22:51:14 +0300

    init

---
+++ src/app.module.ts
@@ -0,0 +1,29 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Module } from '@nestjs/common';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ReportsModule } from './reports/reports.module';
+import { UsersModule } from './users/users.module';
+import { User } from './users/user.entity';
+import { Report } from './reports/report.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'db',
+      entities: [User, Report],
+      synchronize: true,
+    }),
+    ReportsModule,
+    UsersModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
