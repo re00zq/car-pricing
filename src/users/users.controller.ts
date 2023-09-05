e513e8f00293b889f8cad16407d453887afefbd7
Mahmoud <re00zq@gmail.com>
2023-09-05 10:29:31 +0300

    finish 8th module

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+
+import { CreateUserDto } from './dtos/create-user-dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Post('register')
+  createUser(@Body() body: CreateUserDto) {
+    console.log(body);
+  }
+}
