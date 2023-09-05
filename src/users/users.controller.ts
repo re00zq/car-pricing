89071d0ad5c5f3ec19f31688da64bb60ee79addf
Mahmoud <re00zq@gmail.com>
2023-09-05 18:40:18 +0300

    finish 9th module

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,43 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Delete,
+  Patch,
+} from '@nestjs/common';
 
 import { CreateUserDto } from './dtos/create-user-dto';
+import { UsersService } from './users.service';
+import { UpdateUserDto } from './dtos/update-user-dto';
 
 @Controller('users')
 export class UsersController {
+  constructor(private userService: UsersService) {}
+
   @Post('register')
-  createUser(@Body() body: CreateUserDto) {
-    console.log(body);
+  create(@Body() body: CreateUserDto) {
+    return this.userService.create(body);
+  }
+
+  @Get()
+  findAll() {
+    return this.userService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.userService.findOne(parseInt(id));
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.userService.update(parseInt(id), body);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.userService.delete(parseInt(id));
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
+import { User } from './user.entity';
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectRepository(User) private userRepo: Repository<User>) {}
+
+  async findOne(id: number): Promise<User | null> {
+    const user: User = await this.userRepo.findOneBy({ id });
+    if (!user)
+      throw new NotFoundException(`the user with ${id} id is not found`);
+    return user;
+  }
+
+  findAll(): Promise<User[]> {
+    return this.userRepo.find();
+  }
+
+  create(data: Partial<User>): Promise<User> {
+    const user = this.userRepo.create(data);
+    return this.userRepo.save(user);
+  }
+
+  async update(id: number, data: Partial<User>) {
+    const user: User = await this.userRepo.findOneBy({ id });
+    if (!user)
+      throw new NotFoundException(`the user with ${id} id is not found`);
+    return this.userRepo.update(user, data);
+  }
+
+  async delete(id: number) {
+    const user: User = await this.userRepo.findOneBy({ id });
+    if (!user)
+      throw new NotFoundException(
+        `the user with ${id} id is is already deleted`,
+      );
+    return this.userRepo.delete(id);
+  }
+}
