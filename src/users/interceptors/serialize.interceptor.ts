d4bddbc0687ddd407519198f46e7ae9edbcfb837
Mahmoud Rezq <mahmoud.m.rezq@gmail.com>
2023-09-25 18:39:13 +0300

    Update serialize.interceptor.ts

--- src/users/interceptors/serialize.interceptor.ts
+++ src/users/interceptors/serialize.interceptor.ts
@@ -11,6 +11,8 @@ class SerializeInterceptor implements NestInterceptor {
   constructor(private dto: any) {}
 
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    // every code you write here(before handle() method) will be executed before the route handler.
+    // and every code you write in pipe method (after calling the handle() method) will be executed after the route handler.
     return next.handle().pipe(
       map((data: any) => {
         return plainToInstance(this.dto, data, {
